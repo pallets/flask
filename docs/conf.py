3ac4ec670c881668aaedb2c3937bae5e10984988
Armin Ronacher <armin.ronacher@active-4.com>
2010-04-25 17:05:25 +0200

    Beefed up PDF docs.  They actually look like something now.

--- docs/conf.py
+++ docs/conf.py
@@ -172,35 +172,27 @@ htmlhelp_basename = 'Flaskdoc'
 
 # -- Options for LaTeX output --------------------------------------------------
 
-# The paper size ('letter' or 'a4').
-#latex_paper_size = 'letter'
-
-# The font size ('10pt', '11pt' or '12pt').
-#latex_font_size = '10pt'
-
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-  ('index', 'Flask.tex', u'Flask Documentation',
+  ('latexindex', 'Flask.tex', u'Flask Documentation',
    u'Armin Ronacher', 'manual'),
 ]
-
-# The name of an image file (relative to this directory) to place at the top of
-# the title page.
-#latex_logo = None
-
-# For "manual" documents, if this is true, then toplevel headings are parts,
-# not chapters.
-#latex_use_parts = False
-
-# Additional stuff for the LaTeX preamble.
-#latex_preamble = ''
 
 # Documents to append as an appendix to all manuals.
 #latex_appendices = []
 
 # If false, no module index is generated.
-#latex_use_modindex = True
+latex_use_modindex = False
+
+latex_elements = {
+    'fontpkg':      r'\usepackage{mathpazo}',
+    'papersize':    'a4paper',
+    'pointsize':    '12pt',
+    'preamble':     r'\usepackage{flaskstyle}'
+}
+
+latex_additional_files = ['flaskstyle.sty', 'logo.pdf']
 
 
 # -- Options for Epub output ---------------------------------------------------
---
+++ docs/latexindex.rst
@@ -0,0 +1,4 @@
+Flask Documentation
+===================
+
+.. include:: contents.rst
