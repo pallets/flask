a1cd92e459ba5b481d183d3923e8829fec988775
Armin Ronacher <armin.ronacher@active-4.com>
2010-05-28 21:07:06 +0200

    Removed some unused code

--- flask.py
+++ flask.py
@@ -1424,8 +1424,3 @@ current_app = LocalProxy(lambda: _request_ctx_stack.top.app)
 request = LocalProxy(lambda: _request_ctx_stack.top.request)
 session = LocalProxy(lambda: _request_ctx_stack.top.session)
 g = LocalProxy(lambda: _request_ctx_stack.top.g)
-
-
-# script interface to run a development server
-if __name__ == '__main__':
-    sys.exit(main(sys.argv))
