12c2cb5e12895a0d16b37b7a9e9a96d69a34c36e
Dag Odenhall <dag.odenhall@gmail.com>
2010-06-05 07:55:41 +0800

    Typo in docs for Flask.handle_exception

--- flask.py
+++ flask.py
@@ -1301,7 +1301,7 @@ class Flask(_PackageBoundObject):
     def handle_exception(self, e):
         """Default exception handling that kicks in when an exception
         occours that is not catched.  In debug mode the exception will
-        be re-raised immediately, otherwise it is logged an the handler
+        be re-raised immediately, otherwise it is logged and the handler
         for an 500 internal server error is used.  If no such handler
         exists, a default 500 internal server error message is displayed.
 
