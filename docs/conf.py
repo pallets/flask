97d2a198e216b29be5533dfba81525481bc9c1e5
Armin Ronacher <armin.ronacher@active-4.com>
2010-07-17 16:05:06 +0200

    Added fallback for docs

--- docs/conf.py
+++ docs/conf.py
@@ -250,3 +250,17 @@ intersphinx_mapping = {
 }
 
 pygments_style = 'flask_theme_support.FlaskyStyle'
+
+# fall back if theme is not there
+try:
+    __import__('flask_theme_support')
+except ImportError, e:
+    print '-' * 74
+    print 'Warning: Flask themes unavailable.  Building with default theme'
+    print 'If you want the Flask themes, run this command and build again:'
+    print
+    print '  git submodule init'
+    print '-' * 74
+
+    pygments_style = 'tango'
+    html_theme = 'default'
