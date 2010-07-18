27d28dcef4cee8765dcd7a567689d35a41ddcd75
Armin Ronacher <armin.ronacher@active-4.com>
2010-07-18 20:43:50 +0200

    Added Session again to the public API because existing code might import it

--- flask/__init__.py
+++ flask/__init__.py
@@ -23,6 +23,7 @@ from .helpers import url_for, jsonify, json_available, flash, \
 from .globals import current_app, g, request, session, _request_ctx_stack
 from .module import Module
 from .templating import render_template, render_template_string
+from .session import Session
 
 # the signals
 from .signals import signals_available, template_rendered, request_started, \
