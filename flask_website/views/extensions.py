8547fdeecec9326e29c051754c5e1fead373baef
Armin Ronacher <armin.ronacher@active-4.com>
2010-07-23 11:48:31 +0100

    Added support for retreiving extensions as JSON

--- flask_website/views/extensions.py
+++ flask_website/views/extensions.py
@@ -1,11 +1,17 @@
-from flask import Module, render_template
+from flask import Module, render_template, jsonify, request
 from flask_website.listings.extensions import extensions
 
 mod = Module(__name__, url_prefix='/extensions')
 
 
+def wants_json():
+    return request.accept_mimetypes \
+        .best_match(['application/json', 'text/html']) == 'application/json'
+
 @mod.route('/')
 def index():
+    if wants_json():
+        return jsonify(extensions=map(vars, extensions))
     return render_template('extensions/index.html', extensions=extensions)
 
 
