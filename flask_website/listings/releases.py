3ec22fca46a70b1a675216ff5c1b8ea15fc026ab
Armin Ronacher <armin.ronacher@active-4.com>
2010-07-23 16:13:09 +0100

    Added missing file

---
+++ flask_website/listings/releases.py
@@ -0,0 +1,37 @@
+from urlparse import urljoin
+
+
+server = 'http://pypi.python.org/'
+download_path = '/packages/source/F/Flask/Flask-%s.tar.gz'
+detail_path = '/pypi/Flask/%s'
+
+
+class Release(object):
+
+    def __init__(self, version):
+        self.version = version
+
+    def to_json(self):
+        return dict(version=self.version,
+                    download_url=self.download_url,
+                    detail_url=self.detail_url)
+
+    @property
+    def download_url(self):
+        return urljoin(server, download_path % self.version)
+
+    @property
+    def detail_url(self):
+        return urljoin(server, detail_path % self.version)
+
+
+releases = map(Release, [
+    '0.1',
+    '0.2',
+    '0.3',
+    '0.3.1',
+    '0.4',
+    '0.5',
+    '0.5.1',
+    '0.5.2'
+])
