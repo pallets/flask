a3a843999b8f98a96fb44f573515d44648d72bab
Armin Ronacher <armin.ronacher@active-4.com>
2010-08-09 15:16:02 +0200

    normpath is now used before loading templates

--- tests/moduleapp/apps/admin/__init__.py
+++ tests/moduleapp/apps/admin/__init__.py
@@ -7,3 +7,8 @@ admin = Module(__name__, url_prefix='/admin')
 @admin.route('/')
 def index():
     return render_template('admin/index.html')
+
+
+@admin.route('/index2')
+def index2():
+    return render_template('./admin/index.html')
