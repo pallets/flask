7f1b9ec9065b41e0bf39c4492443adac1326577f
Armin Ronacher <armin.ronacher@active-4.com>
2011-07-07 12:34:29 +0200

    Added unlisted extensions

--- flask_website/views/extensions.py
+++ flask_website/views/extensions.py
@@ -1,7 +1,7 @@
 from flask import Module, render_template, jsonify, redirect, \
      url_for
 from flask_website.utils import request_wants_json
-from flask_website.listings.extensions import extensions
+from flask_website.listings.extensions import extensions, unlisted
 
 mod = Module(__name__, url_prefix='/extensions')
 
@@ -10,7 +10,8 @@ mod = Module(__name__, url_prefix='/extensions')
 def index():
     if request_wants_json():
         return jsonify(extensions=[ext.to_json() for ext in extensions])
-    return render_template('extensions/index.html', extensions=extensions)
+    return render_template('extensions/index.html', extensions=extensions,
+                           unlisted=unlisted)
 
 
 @mod.route('/creating/')
