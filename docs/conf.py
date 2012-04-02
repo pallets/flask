f46f7155b27a741081fc13fa7fb1db53e54f5683
Kenneth Reitz <me@kennethreitz.com>
2012-04-01 20:57:50 -0400

    2012

--- docs/conf.py
+++ docs/conf.py
@@ -43,7 +43,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Flask'
-copyright = u'2010, Armin Ronacher'
+copyright = u'2012, Armin Ronacher'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
