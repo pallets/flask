e0b2239c15778d7dd0e7314a68af6c4abb143446
Ron DuPlain <ron.duplain@gmail.com>
2012-04-03 23:30:27 -0400

    Move Flask-Restless out of unlisted.

--- flask_website/listings/extensions.py
+++ flask_website/listings/extensions.py
@@ -474,6 +474,16 @@ extensions = [
         docs='http://packages.python.org/Flask-Shelve/',
         github='jamesls/flask-shelve',
     ),
+    Extension('Flask-Restless', 'Jeffrey Finkelstein',
+        description='''
+            <p>Flask-Restless provides simple generation of ReSTful APIs for
+              database models defined using Flask-SQLAlchemy. The generated
+              APIs send and receive messages in JSON format.
+        ''',
+        docs='http://readthedocs.org/docs/flask-restless/en/latest/',
+        github='jfinkels/flask-restless',
+        approved=False
+    ),
 ]
 
 
@@ -584,16 +594,6 @@ unlisted = [
         notes='''
             Unlisted because duplicates the Flask-SeaSurf extension.
         '''
-    ),
-    Extension('Flask-Restless', 'Jeffrey Finkelstein',
-        description='''
-            <p>Flask-Restless provides simple generation of ReSTful APIs for
-              database models defined using Flask-SQLAlchemy. The generated
-              APIs send and receive messages in JSON format.
-        ''',
-        docs='http://readthedocs.org/docs/flask-restless/en/latest/',
-        github='jfinkels/flask-restless',
-        approved=False
     ),
 ]
 
