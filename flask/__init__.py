b87919348104774e9c959fc00976966e38161129
Armin Ronacher <armin.ronacher@active-4.com>
2012-08-11 02:37:03 +0100

    Set current dev version number to 0.10

--- flask/__init__.py
+++ flask/__init__.py
@@ -10,7 +10,7 @@
     :license: BSD, see LICENSE for more details.
 """
 
-__version__ = '0.9'
+__version__ = '0.10-dev'
 
 # utilities we import from Werkzeug and Jinja2 that are unused
 # in the module but are exported as public interface.
--- setup.py
+++ setup.py
@@ -77,7 +77,7 @@ class run_audit(Command):
 
 setup(
     name='Flask',
-    version='0.9',
+    version='0.10-dev',
     url='http://github.com/mitsuhiko/flask/',
     license='BSD',
     author='Armin Ronacher',
