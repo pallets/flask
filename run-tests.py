611c54c242116d0bd212ad5b7dab7263c3b0ae18
Yu Jian <askingyj@gmail.com>
2013-03-28 16:01:23 +0800

    Using dot will be cleaner.

--- run-tests.py
+++ run-tests.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
 import sys, os
-sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+sys.path.insert(0, os.path.abspath('.'))
 from flask.testsuite import main
 main()
