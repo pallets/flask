ac187bded7339dbf88baa2c6c7d8adddef657cac
Armin Ronacher <armin.ronacher@active-4.com>
2013-06-13 09:36:03 +0100

    This is 0.11-dev

--- setup.py
+++ setup.py
@@ -78,7 +78,7 @@ class run_audit(Command):
 
 setup(
     name='Flask',
-    version='0.10',
+    version='0.11-dev',
     url='http://github.com/mitsuhiko/flask/',
     license='BSD',
     author='Armin Ronacher',
