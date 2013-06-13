41171d11ae37c4dfae79f17e1477b7bf8bdc034a
Daniel Neuhäuser <dasdasich@gmail.com>
2013-06-13 22:16:14 +0200

    Add Python 3.3 classifier

--- setup.py
+++ setup.py
@@ -104,6 +104,7 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ],
