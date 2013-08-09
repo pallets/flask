44e0568002b43bff2c8bb94c9e38cedb0f7eb521
Daniel Neuhäuser <dasdasich@gmail.com>
2013-08-10 00:21:38 +0200

    Fallback to stdlib urlparse
    This fallback is necessary to maintain compatibility with the currently
    lowest support Werkzeug version.

--- flask/helpers.py
+++ flask/helpers.py
@@ -21,11 +21,10 @@ from werkzeug.routing import BuildError
 from functools import update_wrapper
 from warnings import warn
 
-from werkzeug.urls import url_parse
 try:
-    from werkzeug.urls import url_quote
+    from werkzeug.urls import url_quote, url_parse
 except ImportError:
-    from urlparse import quote as url_quote
+    from urlparse import quote as url_quote, urlparse as url_parse
 
 from werkzeug.datastructures import Headers
 from werkzeug.exceptions import NotFound
