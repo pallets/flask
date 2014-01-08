ce10f8489719ef9f881d6d0b969866e9eb5cc41a
Markus Unterwaditzer <markus@unterwaditzer.net>
2014-01-08 14:39:45 +0100

    Fix #941 -- allow errorhandlers for HTTPException
    Originally i intended to rewrite the error handling system to register
    an error handler for each subclass of HTTPException on initialization
    of the Flask object, removing the distinction between HTTP exceptions
    and other exception types. However, with the current behavior of
    preferring the first-registered handler when an error occurs, doing this
    without compromises would require changing this to preferring the
    last-registered handler, a backwards-incompatible change.
    
    The following is a very minimal fix to only make Flask prioritize the
    user's error handlers over others. I am not sure which performance
    implications this has. Maybe a major rewrite of the error handling
    system is still desirable (with more complex prioritization of
    errorhandlers -- something based on "distance" in inheritance tree?),
    but at the moment i don't see a reason to do so, given that the current
    system is primitive enough to be understandable, while still usable.

--- flask/app.py
+++ flask/app.py
@@ -1360,13 +1360,6 @@ class Flask(_PackageBoundObject):
         exc_type, exc_value, tb = sys.exc_info()
         assert exc_value is e
 
-        # ensure not to trash sys.exc_info() at that point in case someone
-        # wants the traceback preserved in handle_http_exception.  Of course
-        # we cannot prevent users from trashing it themselves in a custom
-        # trap_http_exception method so that's their fault then.
-        if isinstance(e, HTTPException) and not self.trap_http_exception(e):
-            return self.handle_http_exception(e)
-
         blueprint_handlers = ()
         handlers = self.error_handler_spec.get(request.blueprint)
         if handlers is not None:
@@ -1375,6 +1368,13 @@ class Flask(_PackageBoundObject):
         for typecheck, handler in chain(blueprint_handlers, app_handlers):
             if isinstance(e, typecheck):
                 return handler(e)
+
+        # ensure not to trash sys.exc_info() at that point in case someone
+        # wants the traceback preserved in handle_http_exception.  Of course
+        # we cannot prevent users from trashing it themselves in a custom
+        # trap_http_exception method so that's their fault then.
+        if isinstance(e, HTTPException) and not self.trap_http_exception(e):
+            return self.handle_http_exception(e)
 
         reraise(exc_type, exc_value, tb)
 
