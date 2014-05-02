d2d8e66130f8aabf8e0e4b80a42352f8ef93c347
Armin Ronacher <armin.ronacher@active-4.com>
2014-05-02 11:46:04 +0100

    Fixed flask tests

--- examples/minitwit/minitwit.py
+++ examples/minitwit/minitwit.py
@@ -49,13 +49,19 @@ def close_database(exception):
         top.sqlite_db.close()
 
 
-@app.cli.command()
-def initdb():
-    """Creates the database tables."""
+def init_db():
+    """Initializes the database."""
     db = get_db()
     with app.open_resource('schema.sql', mode='r') as f:
         db.cursor().executescript(f.read())
     db.commit()
+
+
+@app.cli.command('initdb')
+def initdb_command():
+    """Creates the database tables."""
+    init_db()
+    print('Initialized the database.')
 
 
 def query_db(query, args=(), one=False):
