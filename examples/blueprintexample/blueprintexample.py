eb0c8a6beea766812fb94712c27ed6e1e99d6249
Suraj Patil <thewhitetulip@users.noreply.github.com>
2014-07-03 18:19:09 +0530

    The example does not execute
    With the inclusion of the app.run we now are able to execute the program, it would be great if we'd have even more detailed docs available for the examples

--- examples/blueprintexample/blueprintexample.py
+++ examples/blueprintexample/blueprintexample.py
@@ -5,3 +5,6 @@ app = Flask(__name__)
 app.register_blueprint(simple_page)
 # Blueprint can be registered many times
 app.register_blueprint(simple_page, url_prefix='/pages')
+
+if __name__=='__main__':
+  app.run()
