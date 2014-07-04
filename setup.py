dfff3d3d11e3a5ed68ca8012173496897a37af65
ctava <ChrisTava@clearchannel.com>
2014-07-04 14:41:46 -0400

    clarified comment and version.

--- setup.py
+++ setup.py
@@ -84,7 +84,7 @@ class run_audit(Command):
 
 setup(
     name='Flask',
-    version='0.10.2',
+    version='0.10.2-dev',
     url='http://github.com/mitsuhiko/flask/',
     license='BSD',
     author='Armin Ronacher',
