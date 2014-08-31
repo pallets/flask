b93ab41ed12c3dd0c37fb7a85f1aa93966b46d77
Markus Unterwaditzer <markus@unterwaditzer.net>
2014-08-31 23:59:43 +0200

    Remove testsuite package from setup.py

--- setup.py
+++ setup.py
@@ -92,7 +92,7 @@ setup(
     description='A microframework based on Werkzeug, Jinja2 '
                 'and good intentions',
     long_description=__doc__,
-    packages=['flask', 'flask.ext', 'flask.testsuite'],
+    packages=['flask', 'flask.ext'],
     include_package_data=True,
     zip_safe=False,
     platforms='any',
