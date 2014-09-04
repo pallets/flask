8a83e7a78be587da0d0b348b9bfd32962043c916
Markus Unterwaditzer <markus@unterwaditzer.net>
2014-09-04 16:28:50 +0200

    Remove useless imports

--- tests/test_deprecations.py
+++ tests/test_deprecations.py
@@ -1,14 +1,10 @@
 # -*- coding: utf-8 -*-
 """
     tests.deprecations
-    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    ~~~~~~~~~~~~~~~~~~
 
     Tests deprecation support. Not used currently.
 
     :copyright: (c) 2014 by Armin Ronacher.
     :license: BSD, see LICENSE for more details.
 """
-
-import flask
-import unittest
-from tests import catch_warnings
