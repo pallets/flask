322c10247f79d5dccd9830a4b67f672ff19d19e9
Melvin <melvin0008@gmail.com>
2015-01-17 17:59:01 +0530

    removed config.py

--- examples/shortenmyurl/config.py
+++
@@ -1,7 +0,0 @@
-import os
-import redis
-
-redis_url = os.getenv('REDISTOGO_URL', 'redis://redistogo:9e87a450dd3ef9a69cd9b31c2560a541@greeneye.redistogo.com:11156/')
-
-
-#redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379');
