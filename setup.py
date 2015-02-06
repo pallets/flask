ddf0dfa8952852fb5365a794c4fe998a8ff6ba8f
ThiefMaster <adrian@planetcoding.net>
2015-02-06 17:02:34 +0100

    Use PEP440-compatible dev version

--- setup.py
+++ setup.py
@@ -78,7 +78,7 @@ class run_audit(Command):
 
 setup(
     name='Flask',
-    version='0.11-dev',
+    version='0.11.dev0',
     url='http://github.com/mitsuhiko/flask/',
     license='BSD',
     author='Armin Ronacher',
