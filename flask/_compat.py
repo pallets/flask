c8f19f0afc8d49a68ec969097e6127d17706b6dc
Mathias J. Hennig <mathias@adblockplus.org>
2015-07-22 23:35:13 +0200

    Reimplement function with_metaclass()

--- flask/_compat.py
+++ flask/_compat.py
@@ -56,21 +56,13 @@ else:
 def with_metaclass(meta, *bases):
     # This requires a bit of explanation: the basic idea is to make a
     # dummy metaclass for one level of class instantiation that replaces
-    # itself with the actual metaclass.  Because of internal type checks
-    # we also need to make sure that we downgrade the custom metaclass
-    # for one level to something closer to type (that's why __call__ and
-    # __init__ comes back from type etc.).
+    # itself with the actual metaclass.
     #
     # This has the advantage over six.with_metaclass in that it does not
     # introduce dummy classes into the final MRO.
-    class metaclass(meta):
-        __call__ = type.__call__
-        __init__ = type.__init__
-        def __new__(cls, name, this_bases, d):
-            if this_bases is None:
-                return type.__new__(cls, name, (), d)
-            return meta(name, bases, d)
-    return metaclass('temporary_class', None, {})
+    constructor = lambda c, name, b, dct: meta(name, bases, dct)
+    metaclass = type('with_metaclass', (type,), {'__new__': constructor})
+    return type.__new__(metaclass, 'temporary_class', (object,), {})
 
 
 # Certain versions of pypy have a bug where clearing the exception stack
