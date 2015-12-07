42bc4dd61a1040614755dda38411420ddc95a1a5
Yash Lodha <ylodha@cognam.com>
2015-12-07 15:02:38 +0530

    instance path is set in the following files of tests cases

--- flask/testsuite/test_apps/main_app.py
+++ flask/testsuite/test_apps/main_app.py
@@ -2,4 +2,4 @@ import os
 import flask
 
 # Test Flask initialization with main module.
-app = flask.Flask('__main__',instance_path=os.path.join(os.path.abspath(os.path.dirname('__main__')),'instance'))
+app = flask.Flask('__main__', instance_path=os.path.join(os.path.abspath(os.path.dirname('__main__')),'instance'))
