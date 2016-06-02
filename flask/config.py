d88c08e56f7398206596129036b8f101be11cba0
Jason Brazeal <jsonbrazeal@users.noreply.github.com>
2016-06-02 15:40:59 -0700

    improved documentation for config.from_object (#1870)

--- flask/config.py
+++ flask/config.py
@@ -143,10 +143,12 @@ class Config(dict):
         -   a string: in this case the object with that name will be imported
         -   an actual object reference: that object is used directly
 
-        Objects are usually either modules or classes.
+        Objects are usually either modules or classes. :meth:`from_object`
+        loads only the uppercase attributes of the module/class. A ``dict``
+        object will not work with :meth:`from_object` because the keys of a
+        ``dict`` are not attributes of the ``dict`` class.
 
-        Just the uppercase variables in that object are stored in the config.
-        Example usage::
+        Example of module-based configuration::
 
             app.config.from_object('yourapplication.default_config')
             from yourapplication import default_config
@@ -156,6 +158,9 @@ class Config(dict):
         rather configuration defaults.  The actual config should be loaded
         with :meth:`from_pyfile` and ideally from a location not within the
         package because the package might be installed system wide.
+
+        See :ref:`config-dev-prod` for an example of class-based configuration
+        using :meth:`from_object`.
 
         :param obj: an import name or object
         """
