feb2c7305afde425285ec19028e29fd79a615212
rmn <r.y.t@yandex.ru>
2016-09-04 23:51:59 +0300

    Fix error in send_file (mimetype_filename is not defined)

--- flask/helpers.py
+++ flask/helpers.py
@@ -519,7 +519,7 @@ def send_file(filename_or_fp, mimetype=None, as_attachment=False,
             if attachment_filename is not None:
                 raise ValueError(
                     'Unable to infer MIME-type from filename {!r}, please '
-                    'pass one explicitly.'.format(mimetype_filename)
+                    'pass one explicitly.'.format(attachment_filename)
                 )
             raise ValueError(
                 'Unable to infer MIME-type because no filename is available. '
