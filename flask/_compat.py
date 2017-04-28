5068ff991362b6f4aefe86255c1886509bf672a3
Ayush Kohli <ayushk96@hotmail.com>
2017-04-28 18:23:50 -0500

    No . in identifier

--- flask/_compat.py
+++ flask/_compat.py
@@ -14,7 +14,7 @@ import sys
 
 PY2 = sys.version_info[0] == 2
 
-PY3.6 = sys.version_info[0] == 3 and sys.version_info[1] == 6
+PY36 = sys.version_info[0] == 3 and sys.version_info[1] == 6
 
 _identity = lambda x: x
 
