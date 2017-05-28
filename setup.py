e67b77b5e349497c185ed13e375d9d395bc3cbb4
David Lord <davidism@gmail.com>
2017-05-28 11:52:01 -0700

    update changelog [ci skip]

--- setup.py
+++ setup.py
@@ -99,6 +99,7 @@ setup(
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ],
