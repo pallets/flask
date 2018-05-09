5f619858f258505bcada772eed9bce35035e955b
Andreea Gherman <andreea@passfort.com>
2018-05-09 13:33:11 +0100

    Remove test

--- examples/tutorial/tests/test_blog.py
+++ examples/tutorial/tests/test_blog.py
@@ -49,17 +49,6 @@ def test_exists_required(client, auth, path):
     assert client.post(path).status_code == 404
 
 
-def test_create(client, auth, app):
-    auth.login()
-    assert client.get('/create').status_code == 200
-    client.post('/create', data={'title': 'created', 'body': ''})
-
-    with app.app_context():
-        db = get_db()
-        count = db.execute('SELECT COUNT(id) FROM post').fetchone()[0]
-        assert count == 2
-
-
 def test_update(client, auth, app):
     auth.login()
     assert client.get('/1/update').status_code == 200
