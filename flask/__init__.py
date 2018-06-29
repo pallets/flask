8bf9fcdb0dd838664b136566298ec5920b8de8cb
Gabriel Saldanha <gabrielcrsaldanha@gmail.com>
2018-06-29 01:06:46 -0300

    :art: Use parentheses in preference to backslashes

--- flask/__init__.py
+++ flask/__init__.py
@@ -20,19 +20,19 @@ from jinja2 import Markup, escape
 
 from .app import Flask, Request, Response
 from .config import Config
-from .helpers import url_for, flash, send_file, send_from_directory, \
-     get_flashed_messages, get_template_attribute, make_response, safe_join, \
-     stream_with_context
-from .globals import current_app, g, request, session, _request_ctx_stack, \
-     _app_ctx_stack
-from .ctx import has_request_context, has_app_context, \
-     after_this_request, copy_current_request_context
+from .helpers import (url_for, flash, send_file, send_from_directory,
+     get_flashed_messages, get_template_attribute, make_response, safe_join,
+     stream_with_context)
+from .globals import (current_app, g, request, session, _request_ctx_stack,
+     _app_ctx_stack)
+from .ctx import (has_request_context, has_app_context,
+     after_this_request, copy_current_request_context)
 from .blueprints import Blueprint
 from .templating import render_template, render_template_string
-from .signals import signals_available, template_rendered, request_started, \
-     request_finished, got_request_exception, request_tearing_down, \
-     appcontext_tearing_down, appcontext_pushed, \
-     appcontext_popped, message_flashed, before_render_template
+from .signals import (signals_available, template_rendered, request_started,
+     request_finished, got_request_exception, request_tearing_down,
+     appcontext_tearing_down, appcontext_pushed,
+     appcontext_popped, message_flashed, before_render_template)
 
 # Expose a convenient wrapper around python's builtin json module.
 from .json import jsonify
