05544feeb7a17332b4fcfc70697553181e4926d7
CasterWx <1325200471@qq.com>
2018-12-08 12:45:20 +0800

    add classifiers

--- setup.py
+++ setup.py
@@ -69,6 +69,7 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
         'Topic :: Software Development :: Libraries :: Application Frameworks',
