83fb602af7ac1a5b905dfe737666f9761e4aa3a1
David Lord <davidism@gmail.com>
2019-01-07 09:52:54 -0800

    add changelog for GH-2986

--- flask/app.py
+++ flask/app.py
@@ -1632,6 +1632,16 @@ class Flask(_PackageBoundObject):
         registered error handlers and fall back to returning the
         exception as response.
 
+        .. versionchanged:: 1.0.3
+            ``RoutingException``, used internally for actions such as
+             slash redirects during routing, is not passed to error
+             handlers.
+
+        .. versionchanged:: 1.0
+            Exceptions are looked up by code *and* by MRO, so
+            ``HTTPExcpetion`` subclasses can be handled with a catch-all
+            handler for the base ``HTTPException``.
+
         .. versionadded:: 0.3
         """
         # Proxy exceptions don't have error codes.  We want to always return
@@ -1639,9 +1649,9 @@ class Flask(_PackageBoundObject):
         if e.code is None:
             return e
 
-        # RoutingExceptions such as RequestRedirects are special exceptions
-        # used to trigger routing actions such as redirects, and also behave
-        # like proxy exceptions. We return these unchanged as well.
+        # RoutingExceptions are used internally to trigger routing
+        # actions, such as slash redirects raising RequestRedirect. They
+        # are not raised or handled in user code.
         if isinstance(e, RoutingException):
             return e
 
