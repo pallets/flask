024f0d384cf5bb65c76ac59f8ddce464b2dc2ca1
David Lord <davidism@gmail.com>
2020-04-04 14:36:09 -0700

    move package metadata to setup.cfg

--- examples/javascript/setup.py
+++ examples/javascript/setup.py
@@ -1,21 +1,3 @@
-from setuptools import find_packages
 from setuptools import setup
 
-with open("README.rst", encoding="utf8") as f:
-    readme = f.read()
-
-setup(
-    name="js_example",
-    version="1.0.0",
-    url="https://flask.palletsprojects.com/patterns/jquery/",
-    license="BSD",
-    maintainer="Pallets team",
-    maintainer_email="contact@palletsprojects.com",
-    description="Demonstrates making Ajax requests to Flask.",
-    long_description=readme,
-    packages=find_packages(),
-    include_package_data=True,
-    zip_safe=False,
-    install_requires=["flask"],
-    extras_require={"test": ["pytest", "coverage", "blinker"]},
-)
+setup()
--- setup.py
+++ setup.py
@@ -1,47 +1,14 @@
 import re
 
-from setuptools import find_packages
 from setuptools import setup
-
-with open("README.rst", encoding="utf8") as f:
-    readme = f.read()
 
 with open("src/flask/__init__.py", encoding="utf8") as f:
     version = re.search(r'__version__ = "(.*?)"', f.read()).group(1)
 
+# Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
 setup(
     name="Flask",
     version=version,
-    url="https://palletsprojects.com/p/flask/",
-    project_urls={
-        "Documentation": "https://flask.palletsprojects.com/",
-        "Code": "https://github.com/pallets/flask",
-        "Issue tracker": "https://github.com/pallets/flask/issues",
-    },
-    license="BSD-3-Clause",
-    author="Armin Ronacher",
-    author_email="armin.ronacher@active-4.com",
-    maintainer="Pallets",
-    maintainer_email="contact@palletsprojects.com",
-    description="A simple framework for building complex web applications.",
-    long_description=readme,
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Environment :: Web Environment",
-        "Framework :: Flask",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: BSD License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
-        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
-        "Topic :: Software Development :: Libraries :: Application Frameworks",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-    ],
-    packages=find_packages("src"),
-    package_dir={"": "src"},
-    include_package_data=True,
-    python_requires=">=3.6",
     install_requires=[
         "Werkzeug>=0.15",
         "Jinja2>=2.10.1",
@@ -59,12 +26,5 @@ setup(
             "sphinxcontrib-log-cabinet",
             "sphinx-issues",
         ],
-        "docs": [
-            "sphinx",
-            "pallets-sphinx-themes",
-            "sphinxcontrib-log-cabinet",
-            "sphinx-issues",
-        ],
     },
-    entry_points={"console_scripts": ["flask = flask.cli:main"]},
 )
