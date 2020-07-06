pallets/flask

commit e6178fe489b7828acc2bb8fd4b56a70b11ab6c6a

Handle different sep across os

src/flask/helpers.py
@@ -1000,6 +1000,8 @@ class _PackageBoundObject(object):
 
     @static_folder.setter
     def static_folder(self, value):
+        if value is not None:
+            value = value.rstrip("/\\")
         self._static_folder = value
 
     @property
@@ -1013,7 +1015,7 @@ class _PackageBoundObject(object):
             return self._static_url_path
 
         if self.static_folder is not None:
-            basename = os.path.basename(self.static_folder.rstrip("/"))
+            basename = os.path.basename(self.static_folder)
             return ("/" + basename).rstrip("/")
 
     @static_url_path.setter

commit 7ba35c4d4fe99418cee9c87900bd0d3b38cf5bd0

Restore support for using pathlib.Path for static_folder.
* No longer causes AttributeError: 'PosixPath' object has no
  attribute 'rstrip'.

* This was broken by e6178fe489b7828acc2bb8fd4b56a70b11ab6c6a
  which was released in 1.1.2.

* Add a regression test that now passes.

See #3557.

src/flask/helpers.py
@@ -980,7 +980,7 @@ class _PackageBoundObject:
     @static_folder.setter
     def static_folder(self, value):
         if value is not None:
-            value = value.rstrip("/\\")
+            value = os.fspath(value).rstrip(r"\/")
         self._static_folder = value
 
     @property

tests/test_basic.py
@@ -1406,6 +1406,16 @@ def test_static_url_empty_path_default(app):
     rv.close()
 
 
+@pytest.mark.skipif(sys.version_info < (3, 6), reason="requires Python >= 3.6")
+def test_static_folder_with_pathlib_path(app):
+    from pathlib import Path
+
+    app = flask.Flask(__name__, static_folder=Path("static"))
+    rv = app.test_client().open("/static/index.html", method="GET")
+    assert rv.status_code == 200
+    rv.close()
+
+
 def test_static_folder_with_ending_slash():
     app = flask.Flask(__name__, static_folder="static/")
 