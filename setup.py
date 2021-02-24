2aa174b62a6ee1ff06a79236f56470aca71b6c8a
David Lord <davidism@gmail.com>
2021-02-24 10:09:50 -0800

    move version to setup.cfg

--- setup.py
+++ setup.py
@@ -1,14 +1,8 @@
-import re
-
 from setuptools import setup
-
-with open("src/flask/__init__.py", encoding="utf8") as f:
-    version = re.search(r'__version__ = "(.*?)"', f.read()).group(1)
 
 # Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
 setup(
     name="Flask",
-    version=version,
     install_requires=[
         "Werkzeug>=0.15",
         "Jinja2>=2.10.1",
