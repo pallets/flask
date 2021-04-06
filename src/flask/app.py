3053c7a39d620010d3597369a278d033ebf5f057
David Lord <davidism@gmail.com>
2021-04-06 15:31:28 -0700

    update async docs

--- src/flask/app.py
+++ src/flask/app.py
@@ -1520,12 +1520,13 @@ class Flask(Scaffold):
         return False
 
     def ensure_sync(self, func):
-        """Ensure that the returned function is sync and calls the async func.
+        """Ensure that the function is synchronous for WSGI workers.
+        Plain ``def`` functions are returned as-is. ``async def``
+        functions are wrapped to run and wait for the response.
+
+        Override this method to change how the app runs async views.
 
         .. versionadded:: 2.0
-
-        Override if you wish to change how asynchronous functions are
-        run.
         """
         if iscoroutinefunction(func):
             return run_async(func)
