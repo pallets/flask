aa11c44e6235a789f51287cd106b080cebddcb15
David Lord <davidism@gmail.com>
2021-05-11 14:48:48 -0700

    start version 2.1.0.dev0

--- src/flask/__init__.py
+++ src/flask/__init__.py
@@ -43,4 +43,4 @@ from .signals import template_rendered
 from .templating import render_template
 from .templating import render_template_string
 
-__version__ = "2.0.1.dev0"
+__version__ = "2.1.0.dev0"
