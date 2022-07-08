2f1d1d625697b9eca82dc8b2f46fff24dace63b1
pgjones <philip.graham.jones@googlemail.com>
2022-07-08 14:33:20 +0100

    Add further typing tests
    This should help ensure the app decorators are correctly typed.

---
+++ tests/typing/typing_app_decorators.py
@@ -0,0 +1,38 @@
+from __future__ import annotations
+
+import typing as t
+
+from flask import Flask
+from flask import Response
+
+app = Flask(__name__)
+
+
+@app.after_request
+def after_sync(response: Response) -> Response:
+    ...
+
+
+@app.after_request
+async def after_async(response: Response) -> Response:
+    ...
+
+
+@app.before_request
+def before_sync() -> None:
+    ...
+
+
+@app.before_request
+async def before_async() -> None:
+    ...
+
+
+@app.teardown_appcontext
+def teardown_sync(exc: t.Optional[BaseException]) -> None:
+    ...
+
+
+@app.teardown_appcontext
+async def teardown_async(exc: t.Optional[BaseException]) -> None:
+    ...
