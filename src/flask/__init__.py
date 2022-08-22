212b72a1feeae69739fb50fc5ab11d1c025350bc
David Lord <davidism@gmail.com>
2022-08-22 09:00:33 -0700

    start version 2.2.3

--- src/flask/__init__.py
+++ src/flask/__init__.py
@@ -42,7 +42,7 @@ from .templating import render_template_string as render_template_string
 from .templating import stream_template as stream_template
 from .templating import stream_template_string as stream_template_string
 
-__version__ = "2.2.2"
+__version__ = "2.2.3"
 
 
 def __getattr__(name):
