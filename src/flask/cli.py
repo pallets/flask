8433f62471ae4a455d4c40f0610cbc9a13141bef
Emir Hodzic <emir.hodzich@gmail.com>
2022-09-07 09:11:20 +0200

    Update cli.py

--- src/flask/cli.py
+++ src/flask/cli.py
@@ -24,7 +24,7 @@ from .helpers import get_load_dotenv
 if t.TYPE_CHECKING:
     from .app import Flask
 
-
+#fdsafdsafdas
 class NoAppException(click.UsageError):
     """Raised if an application cannot be found or loaded."""
 
