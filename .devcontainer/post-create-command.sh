a927a243b011f5b28c60e6f946273ba9e85e3e77
emisargent <55098699+emisargent@users.noreply.github.com>
2023-02-10 19:02:43 +0000

    Make script executable

--- .devcontainer/post-create-command.sh
+++ .devcontainer/post-create-command.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
+set -e
 
 # Add user's fork as a remote
 GIT_USER=$(git config user.name)
 git remote add fork https://github.com/${GIT_USER}/flask
+echo "Added remote"
 
 # Create and activate a virtualenv
 python3 -m venv .venv
 . .venv/bin/activate
+echo "Created virtualenv"
 
 # Upgrade pip and setuptools
 python -m pip install --upgrade pip setuptools
+echo "Upgraded setuptools"
 
 # Install the development dependencies, then install Flask in editable mode
 pip install -r requirements/dev.txt && pip install -e .
+echo "Installed dependencies and Flask"
 
 # Install pre-commit hooks
 pre-commit install
+echo "Installed pre-commit hooks"
 
 # Install coverage
 pip install coverage
+echo "Installed coverage"
