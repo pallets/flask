6f14ad76cece4153d759f3882dd9a2523978b36f
emisargent <55098699+emisargent@users.noreply.github.com>
2023-02-10 21:22:22 +0000

    Update welcome text and add conditional for adding remote fork

--- .devcontainer/post-create-command.sh
+++ .devcontainer/post-create-command.sh
@@ -3,7 +3,9 @@ set -e
 
 # Add user's fork as a remote
 GIT_USER=$(git config user.name)
-git remote add fork https://github.com/${GIT_USER}/flask
+if [ ! git remote | grep -q "fork" ]; then
+    git remote add fork https://github.com/${GIT_USER}/flask
+fi
 
 # Create and activate a virtualenv
 python3 -m venv .venv
@@ -11,16 +13,10 @@ python3 -m venv .venv
 
 # Upgrade pip and setuptools
 python -m pip install --upgrade pip setuptools
-echo "Upgraded setuptools"
 
 # Install the development dependencies, then install Flask in editable mode
 pip install -r requirements/dev.txt && pip install -e .
-echo "Installed dependencies and Flask"
 
-# Install pre-commit hooks
+# Install pre-commit hooks and coverage
 pre-commit install
-echo "Installed pre-commit hooks"
-
-# Install coverage
 pip install coverage
-echo "Installed coverage"
