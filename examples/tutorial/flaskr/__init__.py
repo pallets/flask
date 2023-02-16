133b93d337bc0a74da6185062ff696616c92a4f7
Kassandra Keeton <ProsperousHeart@users.noreply.github.com>
2023-02-16 10:11:11 -0600

    update to create_app - if using environment ariables for SECRET_KEY testing is broken when it is outside like originally suggested

--- examples/tutorial/flaskr/__init__.py
+++ examples/tutorial/flaskr/__init__.py
@@ -6,16 +6,19 @@ from flask import Flask
 def create_app(test_config=None):
     """Create and configure an instance of the Flask application."""
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        # a default secret that should be overridden by instance config
-        SECRET_KEY="dev",
-        # store the database in the instance folder
-        DATABASE=os.path.join(app.instance_path, "flaskr.sqlite"),
-    )
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
-        app.config.from_pyfile("config.py", silent=True)
+        if os.path.isfile("config.py"):
+            app.config.from_pyfile("config.py", silent=True)
+        else:
+            app.config.from_mapping(
+                # a default secret that should be overridden by instance config
+                SECRET_KEY="dev",
+                # store the database in the instance folder
+                DATABASE=os.path.join(app.instance_path, "flaskr.sqlite"),
+            )
+
     else:
         # load the test config if passed in
         app.config.update(test_config)
