f5509a4268d9726946aaede3b5cd58e317860f6c
David Lord <davidism@gmail.com>
2023-03-12 08:13:26 -0700

    try waiting for postCreateCommand

--- .devcontainer/post-create-command.sh
+++ .devcontainer/post-create-command.sh
@@ -2,20 +2,12 @@
 set -e
 
 # Add user's fork as a remote
-if ! git remote | grep -q "fork"; then
-    git remote add fork https://github.com/${GITHUB_USER}/flask
-fi
+# if ! git remote | grep -q "fork"; then
+#     git remote add fork https://github.com/${GITHUB_USER}/flask
+# fi
 
-# Create and activate a virtualenv
 python3 -m venv .venv
 . .venv/bin/activate
-
-# Upgrade pip and setuptools
-python -m pip install --upgrade pip setuptools
-
-# Install the development dependencies, then install Flask in editable mode
+pip install --upgrade pip setuptools
 pip install -r requirements/dev.txt && pip install -e .
-
-# Install pre-commit hooks and coverage
 pre-commit install
-pip install coverage
