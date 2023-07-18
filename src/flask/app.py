pallets/flask

commit c4b39ba2f3fede0555f18fc03b0f2499bb0d424b

replace werkzeug.urls with urllib.parse

src/flask/app.py
@@ -11,6 +11,7 @@ from datetime import timedelta
 from itertools import chain
 from threading import Lock
 from types import TracebackType
+from urllib.parse import quote as _url_quote
 
 import click
 from werkzeug.datastructures import Headers
@@ -27,7 +28,6 @@ from werkzeug.routing import RequestRedirect
 from werkzeug.routing import RoutingException
 from werkzeug.routing import Rule
 from werkzeug.serving import is_running_from_reloader
-from werkzeug.urls import url_quote
 from werkzeug.utils import redirect as _wz_redirect
 from werkzeug.wrappers import Response as BaseResponse
 
@@ -2034,7 +2034,8 @@ class Flask(Scaffold):
             return self.handle_url_build_error(error, endpoint, values)
 
         if _anchor is not None:
-            rv = f"{rv}#{url_quote(_anchor)}"
+            _anchor = _url_quote(_anchor, safe="%!#$&'()*+,/:;=?@")
+            rv = f"{rv}#{_anchor}"
 
         return rv
 

src/flask/testing.py
@@ -3,11 +3,11 @@ from contextlib import contextmanager
 from contextlib import ExitStack
 from copy import copy
 from types import TracebackType
+from urllib.parse import urlsplit
 
 import werkzeug.test
 from click.testing import CliRunner
 from werkzeug.test import Client
-from werkzeug.urls import url_parse
 from werkzeug.wrappers import Request as BaseRequest
 
 from .cli import ScriptInfo
@@ -68,7 +68,7 @@ class EnvironBuilder(werkzeug.test.EnvironBuilder):
             if url_scheme is None:
                 url_scheme = app.config["PREFERRED_URL_SCHEME"]
 
-            url = url_parse(path)
+            url = urlsplit(path)
             base_url = (
                 f"{url.scheme or url_scheme}://{url.netloc or http_host}"
                 f"/{app_root.lstrip('/')}"

commit 547f2b7f1ef086a548494e474503dbfb9b189368

Fix using non-strings as anchors
Previously, non-strings passed to _anchor would be converted to strings.
Restore this ability. The logic is the same as in url_quote.

Fixes: c4b39ba2 ("replace werkzeug.urls with urllib.parse")

src/flask/app.py
@@ -1697,6 +1697,8 @@ class Flask(Scaffold):
             return self.handle_url_build_error(error, endpoint, values)
 
         if _anchor is not None:
+            if not isinstance(_anchor, (str, bytes, bytearray)):
+                _anchor = str(_anchor)
             _anchor = _url_quote(_anchor, safe="%!#$&'()*+,/:;=?@")
             rv = f"{rv}#{_anchor}"
 

tests/test_helpers.py
@@ -106,6 +106,7 @@ class TestUrlFor:
             return "42"
 
         assert flask.url_for("index", _anchor="x y") == "/#x%20y"
+        assert flask.url_for("index", _anchor=5) == "/#5"
 
     def test_url_for_with_scheme(self, app, req_ctx):
         @app.route("/")