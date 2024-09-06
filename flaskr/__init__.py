28d50f431a4a3795f4f6ddddbbb4a6dff8541d2e
Jose Palazon <jose@palako.com>
2024-09-06 10:23:15 +0100

    instance to /tmp

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -5,7 +5,8 @@ from flask import Flask
 
 def create_app(test_config=None):
     """Create and configure an instance of the Flask application."""
-    app = Flask(__name__, instance_relative_config=True)
+    # app = Flask(__name__, instance_relative_config=True)
+    app = Flask(__name__, instance_path='/tmp')
     app.config.from_mapping(
         # a default secret that should be overridden by instance config
         SECRET_KEY="dev",
