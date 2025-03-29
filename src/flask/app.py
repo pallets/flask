165af0a090e229e94ccd93896efc15b9fe0488da
David Lord <davidism@gmail.com>
2025-03-29 14:44:59 -0700

    update dev dependencies

--- src/flask/app.py
+++ src/flask/app.py
@@ -265,9 +265,9 @@ class Flask(App):
         # For one, it might be created while the server is running (e.g. during
         # development). Also, Google App Engine stores static files somewhere
         if self.has_static_folder:
-            assert (
-                bool(static_host) == host_matching
-            ), "Invalid static_host/host_matching combination"
+            assert bool(static_host) == host_matching, (
+                "Invalid static_host/host_matching combination"
+            )
             # Use a weakref to avoid creating a reference cycle between the app
             # and the view function (see #3761).
             self_ref = weakref.ref(self)
