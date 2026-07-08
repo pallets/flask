88d49e42148f9b42131afe7413a7abe39de501d3
Mohamed Jaazeer <mohamedjaazeer4@gmail.com>
2026-07-08 15:02:20 +0530

    Support pytest notset sentinel import change

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,11 @@ import sys
 import pytest
 from _pytest import monkeypatch
 
+try:
+    from _pytest.monkeypatch import notset
+except ImportError:
+    from _pytest.compat import NOTSET as notset
+
 from flask import Flask
 from flask.globals import app_ctx as _app_ctx
 
@@ -16,15 +21,15 @@ def _standard_os_environ():
     """
     mp = monkeypatch.MonkeyPatch()
     out = (
-        (os.environ, "FLASK_ENV_FILE", monkeypatch.notset),
-        (os.environ, "FLASK_APP", monkeypatch.notset),
-        (os.environ, "FLASK_DEBUG", monkeypatch.notset),
-        (os.environ, "FLASK_RUN_FROM_CLI", monkeypatch.notset),
-        (os.environ, "WERKZEUG_RUN_MAIN", monkeypatch.notset),
+        (os.environ, "FLASK_ENV_FILE", notset),
+        (os.environ, "FLASK_APP", notset),
+        (os.environ, "FLASK_DEBUG", notset),
+        (os.environ, "FLASK_RUN_FROM_CLI", notset),
+        (os.environ, "WERKZEUG_RUN_MAIN", notset),
     )
 
     for _, key, value in out:
-        if value is monkeypatch.notset:
+        if value is notset:
             mp.delenv(key, False)
         else:
             mp.setenv(key, value)
